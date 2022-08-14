1a6a3d755775ffa37f59c27fa1b9c4a7e4838ec8
niko <npdci@outlook.com>
2022-08-14 20:48:42 +0200

    funktion burgermenü erstellen

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const btn = document.querySelector("button")
+const nav = document.querySelector("nav")
+
+btn.addEventListener("click", ()=>{
+    console.log("click")
+}
+
+)
+
+
+
+/* 
+btn {
+if (btn === "./menu.png") {
+    btn = "./close.png"
+} else {
+    btn = "./menu.png"
+}
+}
+ */
